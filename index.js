01b6ce98ba9ce5828dde6245598ec8151e35e917
chriswayg <chriswayg@gmail.com>
2021-07-20 03:32:16 +0800

    disable the numerous ListItem deprecation warnings

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-/**
- * @format
- */
+import 'react-native-get-random-values';
+import { LogBox } from 'react-native';
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+// deprecations added in version 2.3 and then removed in version 3:
+LogBox.ignoreLogs([
+  "'ListItem.title' prop has been deprecated and will be removed in the next version.",
+  "'ListItem.checkmark' prop has been deprecated and will be removed in the next version.",
+]);
 
 AppRegistry.registerComponent(appName, () => App);
