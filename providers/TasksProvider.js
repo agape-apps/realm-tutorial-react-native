654196ea031df932e6c43aa8a5b9afff2a13b911
chriswayg <chriswayg@gmail.com>
2021-07-20 03:28:51 +0800

    Sync changes in the background for offline-first!

--- providers/TasksProvider.js
+++ providers/TasksProvider.js
@@ -1,7 +1,7 @@
-import React, { useContext, useState, useEffect, useRef } from "react";
-import Realm from "realm";
-import { Task } from "../schemas";
-import { useAuth } from "./AuthProvider";
+import React, { useContext, useState, useEffect, useRef } from 'react';
+import Realm from 'realm';
+import { Task } from '../schemas';
+import { useAuth } from './AuthProvider';
 
 const TasksContext = React.createContext(null);
 
@@ -15,18 +15,36 @@ const TasksProvider = ({ children, projectPartition }) => {
   const realmRef = useRef(null);
 
   useEffect(() => {
+    // You may want to sync changes in the background to display partial data to
+    // the user while the synced realm downloads data from the server, preventing
+    // the user experience from being blocked. We recommend syncing changes in the
+    // background for applications in which the user's device may go offline.
+    // To sync changes in the background, *open a synced realm synchronously*.
+    const OpenRealmBehaviorConfiguration = {
+      type: 'openImmediately',
+    };
+    // Create a Configuration object, which must include the sync property defining
+    // a SyncConfiguration object. Set this OpenRealmBehaviorConfiguration object as
+    // the value for the newRealmFileBehavior and existingRealmFileBehavior fields of
+    // the SyncConfiguration.
     const config = {
       sync: {
         user: user,
         partitionValue: projectPartition,
+        // The behavior to use when this is the first time opening a realm.
+        newRealmFileBehavior: OpenRealmBehaviorConfiguration,
+        // The behavior to use when a realm file already exists locally,LIA Task 4
+        // i.e. you have previously opened the realm.
+        existingRealmFileBehavior: OpenRealmBehaviorConfiguration,
       },
     };
+
     // open a realm for this particular project
     Realm.open(config).then((projectRealm) => {
       realmRef.current = projectRealm;
 
-      const syncTasks = projectRealm.objects("Task");
-      let sortedTasks = syncTasks.sorted("name");
+      const syncTasks = projectRealm.objects('Task');
+      let sortedTasks = syncTasks.sorted('name');
       setTasks([...sortedTasks]);
       sortedTasks.addListener(() => {
         setTasks([...sortedTasks]);
@@ -49,9 +67,9 @@ const TasksProvider = ({ children, projectPartition }) => {
     projectRealm.write(() => {
       // Create a new task in the same partition -- that is, in the same project.
       projectRealm.create(
-        "Task",
+        'Task',
         new Task({
-          name: newTaskName || "New Task",
+          name: newTaskName || 'New Task',
           partition: projectPartition,
         })
       );
@@ -82,7 +100,7 @@ const TasksProvider = ({ children, projectPartition }) => {
     const projectRealm = realmRef.current;
     projectRealm.write(() => {
       projectRealm.delete(task);
-      setTasks([...projectRealm.objects("Task").sorted("name")]);
+      setTasks([...projectRealm.objects('Task').sorted('name')]);
     });
   };
 
@@ -109,7 +127,7 @@ const TasksProvider = ({ children, projectPartition }) => {
 const useTasks = () => {
   const task = useContext(TasksContext);
   if (task == null) {
-    throw new Error("useTasks() called outside of a TasksProvider?"); // an alert is not placed because this is an error for the developer not the user
+    throw new Error('useTasks() called outside of a TasksProvider?'); // an alert is not placed because this is an error for the developer not the user
   }
   return task;
 };
