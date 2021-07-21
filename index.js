0ce2c6319de39fa9bdde8f522cfbaca8b2001fe8
chriswayg <chriswayg@gmail.com>
2021-07-22 00:25:55 +0800

    fixed warning from bson. suppressed dozens of ListItem warnings

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-/**
- * @format
- */
+// fix warning from bson
+import 'react-native-get-random-values';
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+// suppress react-native-elements deprecation warnings (since version 2.3)
+LogBox.ignoreLogs([
+  "'ListItem.title' prop has been deprecated and will be removed in the next version.",
+  "'ListItem.checkmark' prop has been deprecated and will be removed in the next version.",
+]);
 
 AppRegistry.registerComponent(appName, () => App);
