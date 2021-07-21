70ef013be34d32a8db8cf34f8a74043d7401d8d0
chriswayg <chriswayg@gmail.com>
2021-07-21 21:51:51 +0800

    use tasktracker4 Oregon server

--- getRealmApp.js
+++ getRealmApp.js
@@ -11,7 +11,7 @@ let app;
 // https://docs.mongodb.com/realm-sdks/js/latest/Realm.App.html
 export default function getRealmApp() {
   if (app === undefined) {
-    const appId = 'tasktracker-klsjn'; // Set Realm app ID here.
+    const appId = 'tasktracker4-qdhcl'; // Set Realm app ID here.
     const appConfig = {
       id: appId,
       timeout: 10000,
